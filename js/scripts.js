2a5b7d0a46a01ef69f6268777aca09a1738d297e
agatakolohe <agatakolohe@gmail.com>
2020-09-11 21:52:45 -0700

    create function for capitalize first and last letter in string

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
-
-
 function capitalizeFirstLetter(str) {
-  let capital = str.charAt(0,2).toUpperCase() + str.slice(1);
-  return capital;
+ let capital = str.charAt().toUpperCase() + str.charAt(str.length-1).toUpperCase();
+ let reverse = capital.reverse(); 
+ return string;
 }
+
+//this function makes the first letter uppercase//
+//function capitalizeFirstLetter(str) {
+ // let capital = str.charAt().toUpperCase() + str.slice(1);
+ // return capital;
+//}
+
+
+
+
 let string = prompt("Type a sentence");
 let result = capitalizeFirstLetter(string);
 console.log(result);
